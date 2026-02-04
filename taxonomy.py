a26978178c2fb78212888e91f5b61b254d0dd99f
Akshay Kumar A <akshaykumaraksh003@gmail.com>
2026-02-04 12:22:44 +0530

    Update taxonomy.py

--- taxonomy.py
+++ taxonomy.py
@@ -1,64 +1,10 @@
-TAXONOMY="""L1
-L2
-L3
-Banking & Financial
-Banking Charges
-Banking & Financial
-Global Rating
-Banking & Financial
-Insurance
-Facilities
-Food Services
-Facilities
-Janitorial Services
-Facilities
-Security Services
-Facilities
-Uniform
-HR
-Employee Benefits
-HR
-Employee Recognition
-HR
-Recruitment Services
-HR
-Training
-IT
-Hardware
-Accessories
-IT
-Hardware
-Laptop
-IT
-Hardware
-Mobile
-IT
-Software
-Licenses Cost
-IT
-Software
-Subscription
-Professional Services
-Audit Services
-Professional Services
-Professional Services
-Consulting Services
-Professional Services
-Legal Services
-T&E
-Risk Consulting Services
-T&E
-Air
-T&E
-Food
-T&E
-GROUND TRANSPORTATION
-T&E
-Hotel
-Parking fees
-Unaddressable
-Utilities
-Tax
-Power
-Utilities
-Water"""
+TAXONOMY = """
+L1, L2, L3
+IT, Software, Subscription
+IT, Software, Licenses Cost
+IT, Hardware, Laptop
+IT, Hardware, Accessories
+Facilities, Janitorial Services, Not sure
+HR, Recruitment Services, Not sure
+Professional Services, Legal Services, Not sure
+"""
